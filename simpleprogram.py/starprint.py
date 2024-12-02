e40accdf3960c18aa0f7ba04f060cdf425a6eb16
A.AHAMED <Asfak4616>
2024-12-02 23:52:43 +0530

    modify code

--- simpleprogram.py/starprint.py
+++ simpleprogram.py/starprint.py
@@ -4,7 +4,7 @@ for i in range (1,rows+1):
     for j in range(1,(rows-i)+1):
         print(end=" ")
     while(k!=2*i-1):
-        print("* ",end=" ")
+        print("*",end="")
         k+=1
     k=0
     print()
